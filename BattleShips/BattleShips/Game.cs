47f1b6c7f6b0fdf3740c9dec4df904b4ed63a941
Ilya Valchanka <ilia.valchenko.epam@gmail.com>
2017-07-31 18:38:46 +0300

    Add VS project.

---
+++ BattleShips/BattleShips/Game.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace BattleShips
+{
+    public static class Game
+    {
+        public static Dictionary<string, double> damagedOrSunk(int[,] board, int[,] attacks)
+        {
+            Dictionary<string, double> result = new Dictionary<string, double>
+            {
+                {"sunk", 0},
+                { "damaged", 0},
+                { "notTouched", 0},
+                { "points", 0}
+            };
+
+            // number, size
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+            Dictionary<int, int> damageDictionary = new Dictionary<int, int>();
+            int boardRows = board.GetLength(0);
+
+            foreach (var position in board)
+            {
+                // постараться все сделать за один проход
+                if (position != 0)
+                {
+                    if (dictionary.ContainsKey(position))
+                    {
+                        dictionary[position]++;
+                    }
+                    else
+                    {
+                        dictionary.Add(position, 1);
+                    }
+                }
+            }
+
+            for (int i = 0; i < attacks.Length / 2; i++)
+            {
+                int x = boardRows - attacks[i, 1];
+                int y = attacks[i, 0] - 1;
+                int boardValue = board[x, y];
+
+                if (boardValue != 0)
+                {
+                    if (damageDictionary.ContainsKey(boardValue))
+                    {
+                        damageDictionary[boardValue]++;
+                    }
+                    else
+                    {
+                        damageDictionary.Add(boardValue, 1);
+                    }
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                if (!damageDictionary.ContainsKey(item.Key))
+                {
+                    // если такого ключа нет, то корабля не поврежден
+                    result["notTouched"]++;
+                    result["points"]--;
+                    continue;
+                }
+
+                if (item.Value - damageDictionary[item.Key] == 0)
+                {
+                    result["sunk"]++;
+                    result["points"]++;
+                    continue;
+                }
+
+
+                    result["damaged"]++;
+                    result["points"] += 0.5;
+                    continue;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BattleShips/BattleShips/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BattleShips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // System.IndexOutOfRangeException : Index was outside the bounds of the array.
+
+            // -------------  1 ---------------
+
+            //0,0,0,0,0,0
+            //0,0,0,0,0,0
+            //0,1,2,2,0,0
+            //0,1,0,0,0,0
+            //0,0,0,0,0,0
+            //0,0,0,0,0,0
+            //0,0,0,0,0,0
+
+            ////Attacks
+            //(4, 3)(4, 1)(2, 2)(4, 2)(1, 2)
+
+            // -------------- 2 ----------------
+
+            //0,0,0,0,0
+            //0,0,0,0,0
+            //0,0,0,0,0
+            //0,2,2,0,0
+            //0,0,0,0,0
+            //0,0,0,0,0
+            //0,0,0,0,0
+
+            ////Attacks
+            //(4, 1)(1, 2)(1, 1)(3, 1)(2, 1)(3, 2)(4, 2)(2, 2)
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            int[,] board = { { 3, 0, 1 },
+                             { 3, 0, 1 },
+                             { 0, 2, 1 },
+                             { 0, 2, 0 } };
+            int[,] attacks = { { 2, 1 }, { 2, 2 }, { 3, 2 }, { 3, 3 } };
+
+            var result = Game.damagedOrSunk(board, attacks);
+
+            stopwatch.Stop();
+
+            Console.WriteLine("RESULT:\n");
+
+            foreach (var r in result)
+            {
+                Console.Write($"[{r.Key}, {r.Value}], ");
+            }
+
+            Console.WriteLine($"\n\nElapsed time: {stopwatch.Elapsed}");
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
